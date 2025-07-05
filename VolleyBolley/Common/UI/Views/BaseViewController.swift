d237fdcaceecf6b441a319925669f2a9b0b91173
Nikolai Eremenko <nikolai.dev@icloud.com>
2025-07-05 22:37:58 +0400

    [VB-20] - добавлен кастомный navbar

--- VolleyBolley/Common/UI/Views/BaseViewController.swift
+++ VolleyBolley/Common/UI/Views/BaseViewController.swift
@@ -14,25 +14,47 @@ import UIKit
 /// configures the navigation bar to be hidden. Subclasses can override or extend these behaviors
 /// as needed.
 /// 
-/// - Important: This class automatically hides the navigation bar and sets the screen background color using `AppColor.Background.screen`.
+/// - Important: This class automatically hides the navigation bar
+/// and sets the screen background color using `AppColor.Background.screen`.
 ///
 /// - SeeAlso: `UIViewController`
 class BaseViewController: UIViewController {
+    private lazy var navBar: CustomNavBarView = {
+        let view = CustomNavBarView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureBaseUI()
-    }
-
-    private func configureBaseUI() {
-        view.backgroundColor = AppColor.Background.screen
-
-        navigationController?.setNavigationBarHidden(true, animated: false)
+        setupLayout()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
         navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+    private func configureBaseUI() {
+        view.backgroundColor = AppColor.Background.screen
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+    private func setupLayout() {
+        view.addSubview(navBar)
+        setupNavBarConstraints()
+    }
+
+    // MARK: - Constraints
+    private func setupNavBarConstraints() {
+        NSLayoutConstraint.activate([
+            navBar.topAnchor.constraint(equalTo: view.topAnchor),
+            navBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            navBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            navBar.heightAnchor.constraint(equalToConstant: 106)
+        ])
     }
 }
