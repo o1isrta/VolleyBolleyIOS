8e4d1ca12f349095d1633f366e2fd1413800def4
Oleg K <tup1k@yandex.ru>
2025-07-12 21:38:30 +0300

    [VM-21-10] Добавлены кастомные кнопки, заголовки и разделитель

---
+++ VolleyBolley/Common/UI/Views/CustomSeparator.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class CustomSeparator: UIView {
+    
+    /*
+     Разделитель для случаев когда нет смысла
+     использовать коллекции
+     */
+    
+    init() {
+        super.init(frame: .zero)
+        self.backgroundColor = AppColor.Border.separator
+        self.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
