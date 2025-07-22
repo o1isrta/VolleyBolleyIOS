c645a70d40f026aabd9a47f7e41fd627338284ac
EntityVadim <21930@mail.ru>
2025-07-22 19:33:51 +0300

    [VB-23] - Добавил бэкграунд алерта и удалил лишние строки кода

--- VolleyBolley/Common/Style/AppEffect.swift
+++ VolleyBolley/Common/Style/AppEffect.swift
@@ -5,7 +5,16 @@
 //  Created by Nikolai Eremenko
 //
 
-import Foundation
+import UIKit
 
 /// Эффекты такие как эффект стекла, блюры и прочие
-enum AppEffect {}
+enum AppEffect {
+
+    enum BackgroundAlert {
+        static let alert = semiTransparentGray
+    }
+}
+
+private extension AppEffect {
+    static let semiTransparentGray = UIColor(hex: "#555252", alpha: 0.3)
+}
