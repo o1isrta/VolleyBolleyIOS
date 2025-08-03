b41e3e9233ea32af8855d4901f9a4d8d44cfafa0
Roman Romanov <rromanov.vk@gmail.com>
2025-08-03 13:27:24 +0500

    [VB-22] adding GameDescriptionView

--- VolleyBolley/Common/Extensions/Date+UI.swift
+++ VolleyBolley/Common/Extensions/Date+UI.swift
@@ -9,7 +9,28 @@ import Foundation
 
 extension Date {
 
-    func localizedIntervalString(to endDate: Date) -> String {
-        AppDateFormatters.localizedInterval.string(from: self, to: endDate)
-    }
+	func localizedIntervalString(to endDate: Date) -> String {
+		AppDateFormatters.localizedInterval.string(from: self, to: endDate)
+	}
+
+	static func formatDateRange(startString: String, endString: String) -> String? {
+		let inputFormatter = AppDateFormatters.apiDateOnly
+		print("startDate", startString)
+		print("endDate", endString)
+		guard
+			let startDate = inputFormatter.date(from: startString),
+			let endDate = inputFormatter.date(from: endString)
+		else {
+			return nil
+		}
+		print("startDate", startDate)
+		print("endDate", endDate)
+		let startDateFormatter = AppDateFormatters.dateWithTime
+		let endTimeFormatter = AppDateFormatters.time
+
+		let startTime = startDateFormatter.string(from: startDate)
+		let endTime = endTimeFormatter.string(from: endDate).lowercased()
+
+		return "\(startTime)-\(endTime)"
+	}
 }
