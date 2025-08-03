8b63d15ab94383c88b0bf2f8524784ed0279c9b9
Roman Romanov <rromanov.vk@gmail.com>
2025-08-03 17:03:24 +0500

    [VB-18] fixes after merge with VB-22-CourtView + translations

--- VolleyBolley/Modules/Map/CourtList/Cells/CourtDetailsCell.swift
+++ VolleyBolley/Modules/Map/CourtList/Cells/CourtDetailsCell.swift
@@ -9,42 +9,51 @@ import UIKit
 
 class CourtDetailsCell: UITableViewCell {
 
-    // MARK: - Private Properties
+	// MARK: - Private Properties
 
 	private let courtView = CourtView()
 
-    // MARK: - Initializers
+	// MARK: - Initializers
 
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupUI()
-    }
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		setupUI()
+	}
 
 	@available(*, unavailable)
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
 
-    // MARK: - Public Methods
+	// MARK: - Public Methods
 
-    func configure(with court: CourtModel) {
-		courtView.configure(with: court, courtViewType: .oneSmallButton)
-    }
+	func configure(with court: CourtModel) {
+		courtView.configure(
+			with: court,
+			doneButtonData: CourtButtonData(
+				title: "CHOOSE THIS COURT",
+				action: { print("CHOOSE THIS COURT: click clack") }
+			),
+			courtButtonsViewType: .oneSmallButton
+		)
+	}
 }
 
 // MARK: - Private Methods
 
 private extension CourtDetailsCell {
 
-    func setupUI() {
-        selectionStyle = .none
+
+
+	func setupUI() {
+		selectionStyle = .none
 		backgroundColor = .clear
-        contentView.addSubviews(courtView)
-        NSLayoutConstraint.activate([
+		contentView.addSubviews(courtView)
+		NSLayoutConstraint.activate([
 			courtView.topAnchor.constraint(equalTo: contentView.topAnchor),
 			courtView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
 			courtView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
 			courtView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
-        ])
-    }
+		])
+	}
 }
