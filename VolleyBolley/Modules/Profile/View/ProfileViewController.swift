56b4a23d9bad2614f1e43cfdbd3081a552c1a23f
EntityVadim <21930@mail.ru>
2025-08-07 14:20:11 +0400

    [VB-41] - Верстка экран настроек профиля игрока

--- VolleyBolley/Modules/Profile/View/ProfileViewController.swift
+++ VolleyBolley/Modules/Profile/View/ProfileViewController.swift
@@ -20,12 +20,28 @@ final class ProfileViewController: BaseViewController, ProfileViewProtocol {
     private let presenter: ProfilePresenterProtocol
 
     private lazy var navigationBarView = CustomNavBarView()
+    private lazy var mainTabBarController = MainTabBarController()
 
     private lazy var label: UILabel = {
         let view = UILabel()
         view.textAlignment = .center
         view.font = AppFont.Quantex.regular(size: 16)
         return view
+    }()
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .black
+        tableView.layer.cornerRadius = 32
+        return tableView
+    }()
+
+    private lazy var deleteButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle(String(localized: "DELETE ACCOUNT"), for: .normal)
+        button.setTitleColor(AppColor.Text.primary, for: .normal)
+        button.titleLabel?.font = AppFont.Hero.light(size: 14)
+        return button
     }()
 
     // MARK: - Initializers
@@ -62,39 +78,72 @@ final class ProfileViewController: BaseViewController, ProfileViewProtocol {
     func displayError(message: String) {
         print(message)
     }
+}
 
-    // MARK: - Private Methods
+// MARK: - Constants
 
-    private func setupView() {
-        view.addSubview(navigationBarView)
-        view.addSubview(label)
-        setupLayout()
+private extension ProfileViewController {
+    func setupUI() {
+        [navigationBarView, label, tableView, deleteButton].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+
+        addChild(mainTabBarController)
+        view.addSubview(mainTabBarController.view)
+        mainTabBarController.didMove(toParent: self)
+        mainTabBarController.view.translatesAutoresizingMaskIntoConstraints = false
     }
 
-    private func setupLayout() {
-        setupConstraintsNavBar()
-        setupConstraintsLabel()
-    }
-
-    // MARK: - Constraints
-
-    private func setupConstraintsNavBar() {
-        navigationBarView.translatesAutoresizingMaskIntoConstraints = false
+    func setupView() {
+        setupUI()
 
         NSLayoutConstraint.activate([
             navigationBarView.topAnchor.constraint(equalTo: view.topAnchor),
             navigationBarView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             navigationBarView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            navigationBarView.heightAnchor.constraint(equalToConstant: 106)
-        ])
-    }
+            navigationBarView.heightAnchor.constraint(equalToConstant: 106),
 
-    private func setupConstraintsLabel() {
-        label.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+
+            tableView.topAnchor.constraint(equalTo: navigationBarView.bottomAnchor, constant: 8),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            tableView.heightAnchor.constraint(equalToConstant: 400),
+
+            deleteButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 28),
+            deleteButton.bottomAnchor.constraint(equalTo: mainTabBarController.view.topAnchor, constant: -20),
+
+            mainTabBarController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mainTabBarController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mainTabBarController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mainTabBarController.view.heightAnchor.constraint(equalToConstant: 81)
         ])
     }
 }
+
+#if DEBUG
+import SwiftUI
+
+struct ProfileViewControllerPreview: UIViewControllerRepresentable {
+    class StubPresenter: ProfilePresenterProtocol {
+        weak var view: ProfileViewProtocol?
+        func viewDidLoad() {}
+    }
+
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let presenter = StubPresenter()
+        return ProfileViewController(presenter: presenter)
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+}
+
+struct ProfileViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileViewControllerPreview()
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+#endif
