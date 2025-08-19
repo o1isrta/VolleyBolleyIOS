b716e9a4f8dc1936f6d5f987044fdb18f78125f2
Roman Romanov <rromanov.vk@gmail.com>
2025-08-19 20:35:08 +0500

    [VB-18] Dynamic tableView height

--- VolleyBolley/Modules/Map/CourtList/CourtListViewController.swift
+++ VolleyBolley/Modules/Map/CourtList/CourtListViewController.swift
@@ -10,30 +10,32 @@ import UIKit
 
 class CourtListViewController: UIViewController {
 
-    // MARK: - Private Properties
+	// MARK: - Private Properties
 
-    private let presenter = CourtListPresenter()
-    private let interactor: CourtListInteractor
-    private let locationManager = CLLocationManager()
+	private let presenter = CourtListPresenter()
+	private let interactor: CourtListInteractor
+	private let locationManager = CLLocationManager()
 
 	private var courtList: [(court: CourtModel, distance: Double)] = [] {
 		didSet {
 			updateFilteredCourts()
 		}
 	}
-    private var filteredCourts: [(court: CourtModel, distance: Double)] = []
+	private var filteredCourts: [(court: CourtModel, distance: Double)] = []
 
-    private var selectedCourt: CourtModel?
-    private var initialCourts: [CourtModel] = []
-    private var expandedIndex: Int?
+	private var selectedCourt: CourtModel?
+	private var initialCourts: [CourtModel] = []
+	private var expandedIndex: Int?
 
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.separatorStyle = .none
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 80
-        return tableView
-    }()
+	private lazy var tableView: UITableView = {
+		let tableView = UITableView()
+		tableView.separatorStyle = .none
+		tableView.rowHeight = UITableView.automaticDimension
+		tableView.estimatedRowHeight = 80
+		tableView.showsVerticalScrollIndicator = false
+		return tableView
+	}()
+	private var heightConstraint: NSLayoutConstraint!
 
 	private lazy var searchField = GradientSearchField(type: .search)
 	private lazy var glassmorphismView: GlassmorphismView = {
@@ -42,168 +44,187 @@ class CourtListViewController: UIViewController {
 		return view
 	}()
 
-    // MARK: - Initializers
+	// MARK: - Initializers
 
 	init(courts: [CourtModel], selected: CourtModel?) {
-        self.interactor = CourtListInteractor(courts: courts, distanceService: DistanceService())
-        super.init(nibName: nil, bundle: nil)
-        self.selectedCourt = selected
-        self.initialCourts = courts
-        // Store initial courts but don't set distances yet
-        self.courtList = courts.map { ($0, -1) }
+		self.interactor = CourtListInteractor(courts: courts, distanceService: DistanceService())
+		super.init(nibName: nil, bundle: nil)
+		self.selectedCourt = selected
+		self.initialCourts = courts
+		// Store initial courts but don't set distances yet
+		self.courtList = courts.map { ($0, -1) }
 
-        print("🏀 ListViewController: Initialized with \(courts.count) courts") // TODO
-        for (index, court) in courts.enumerated() {
+		print("🏀 ListViewController: Initialized with \(courts.count) courts") // TODO
+		for (index, court) in courts.enumerated() {
 			print("🏀 Court \(index + 1): '\(court.location.courtName)' at \(court.location.latitude), \(court.location.longitude)")
-        }
-    }
+		}
+	}
 
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
 
-    // MARK: - Public Methods
+	// MARK: - Public Methods
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        presenter.view = self
-        presenter.interactor = interactor
-        setupTable()
-        setupUI()
-        setupLocation()
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		presenter.view = self
+		presenter.interactor = interactor
+		setupTable()
+		setupUI()
+		setupLocation()
 		setupSearchTextField()
-    }
+
+		// Dynamic height update
+		tableView.addObserver(self, forKeyPath: "contentSize", options: .new, context: nil)
+	}
+
+	override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+		if keyPath == "contentSize", object as? UITableView == tableView {
+			if let newSize = change?[.newKey] as? CGSize {
+				// Limiting the max height to preserve scrolling
+				let maxHeight = UIScreen.main.bounds.height - 200
+				let newHeight = min(newSize.height, maxHeight)
+
+				heightConstraint.constant = newHeight
+			}
+		}
+	}
+
+	deinit {
+		tableView.removeObserver(self, forKeyPath: "contentSize")
+	}
 }
 
 extension CourtListViewController: CourtListViewProtocol {
 
-    func showCourts(_ courts: [(court: CourtModel, distance: Double)]) {
-        print("📱 ListViewController: Updating courts with distances")
-        for (index, court) in courtList.enumerated() {
+	func showCourts(_ courts: [(court: CourtModel, distance: Double)]) {
+		print("📱 ListViewController: Updating courts with distances")
+		for (index, court) in courtList.enumerated() {
 			print("📱 Court \(index + 1): '\(court.court.location.courtName)' - \(court.distance) km")
-        }
-        courtList = courts
-        tableView.reloadData()
-    }
+		}
+		courtList = courts
+		tableView.reloadData()
+	}
 }
 
 // MARK: - UITableViewDelegate
 
 extension CourtListViewController: UITableViewDelegate {
 
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let realIndex = isRealIndex(row: indexPath.row)
-        expandedIndex = expandedIndex == realIndex ? nil : realIndex
-        tableView.reloadData()
-    }
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let realIndex = isRealIndex(row: indexPath.row)
+		expandedIndex = expandedIndex == realIndex ? nil : realIndex
+		tableView.reloadData()
+	}
 
-    private func handleCourtSelection(_ selectedCourt: CourtModel) {
-        print("handleCourtSelection: \(selectedCourt)")
-    }
+	private func handleCourtSelection(_ selectedCourt: CourtModel) {
+		print("handleCourtSelection: \(selectedCourt)")
+	}
 
-    private func isRealIndex(row: Int) -> Int {
-        let realIndex = expandedIndex != nil && row > expandedIndex ?? 0
-            ? row - 1
-            : row
+	private func isRealIndex(row: Int) -> Int {
+		let realIndex = expandedIndex != nil && row > expandedIndex ?? 0
+		? row - 1
+		: row
 
-        return realIndex
-    }
+		return realIndex
+	}
 }
 
 // MARK: - UITableViewDataSource
 
 extension CourtListViewController: UITableViewDataSource {
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		getRowsCount()
-    }
+	}
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let expanded = expandedIndex, indexPath.row == expanded + 1 {
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		if let expanded = expandedIndex, indexPath.row == expanded + 1 {
 			if let cell = tableView.dequeueReusableCell(
 				withIdentifier: CourtDetailsCell.reuseIdentifier,
 				for: indexPath
 			) as? CourtDetailsCell {
-                let court = filteredCourts[expanded].court
+				let court = filteredCourts[expanded].court
 				let isLast = indexPath.row == getRowsCount() - 1
 				cell.configure(with: court, isLast: isLast)
-                return cell
-            }
-        }
+				return cell
+			}
+		}
 
 		if let cell = tableView.dequeueReusableCell(
 			withIdentifier: CourtTableViewCell.reuseIdentifier,
 			for: indexPath
 		) as? CourtTableViewCell {
-            let realIndex = isRealIndex(row: indexPath.row)
+			let realIndex = isRealIndex(row: indexPath.row)
 			if filteredCourts.indices.contains(realIndex) {
 				let item = filteredCourts[realIndex]
 				let isLast = indexPath.row == getRowsCount() - 1 || indexPath.row == expandedIndex ? true : false
 				cell.configure(with: item.court, distance: item.distance, isLast: isLast)
 				return cell
 			}
-        }
+		}
 
-        return UITableViewCell()
-    }
+		return UITableViewCell()
+	}
 }
 
 // MARK: - CLLocationManagerDelegate
 
 extension CourtListViewController: CLLocationManagerDelegate {
 
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print("📍 LocationManager: Got location update")// TODO
-        if let location = locations.first {
-            print("📍 Location: \(location.coordinate.latitude), \(location.coordinate.longitude)")
-        }
-        // Use initial courts for distance calculation
-        presenter.updateDistancesForCourts(initialCourts, userLocation: locations.first)
-    }
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		print("📍 LocationManager: Got location update")// TODO
+		if let location = locations.first {
+			print("📍 Location: \(location.coordinate.latitude), \(location.coordinate.longitude)")
+		}
+		// Use initial courts for distance calculation
+		presenter.updateDistancesForCourts(initialCourts, userLocation: locations.first)
+	}
 
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("❌ LocationManager: Failed to get location - \(error.localizedDescription)")
+	func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+		print("❌ LocationManager: Failed to get location - \(error.localizedDescription)")
 
-        // Детальная обработка ошибок
-        if let clError = error as? CLError {
-            switch clError.code {
-            case .denied:
-                print("❌ Пользователь запретил доступ к геолокации")
-                // Для тестирования используем тестовую локацию (Нью-Йорк)
-                let testLocation = CLLocation(latitude: 40.7589, longitude: -73.9851)
-                print("📍 Используем тестовую локацию: \(testLocation.coordinate.latitude), \(testLocation.coordinate.longitude)")
-                presenter.updateDistancesForCourts(initialCourts, userLocation: testLocation)
-                return
-            case .locationUnknown:
-                print("❌ Локация временно недоступна")
-            case .network:
-                print("❌ Проблема с сетью")
-            default:
-                print("❌ Ошибка геолокации: \(clError.localizedDescription)")
-            }
-        }
+		// Детальная обработка ошибок
+		if let clError = error as? CLError {
+			switch clError.code {
+			case .denied:
+				print("❌ Пользователь запретил доступ к геолокации")
+				// Для тестирования используем тестовую локацию (Нью-Йорк)
+				let testLocation = CLLocation(latitude: 40.7589, longitude: -73.9851)
+				print("📍 Используем тестовую локацию: \(testLocation.coordinate.latitude), \(testLocation.coordinate.longitude)")
+				presenter.updateDistancesForCourts(initialCourts, userLocation: testLocation)
+				return
+			case .locationUnknown:
+				print("❌ Локация временно недоступна")
+			case .network:
+				print("❌ Проблема с сетью")
+			default:
+				print("❌ Ошибка геолокации: \(clError.localizedDescription)")
+			}
+		}
 
-        // Use initial courts for distance calculation
-        presenter.updateDistancesForCourts(initialCourts, userLocation: nil)
-    }
+		// Use initial courts for distance calculation
+		presenter.updateDistancesForCourts(initialCourts, userLocation: nil)
+	}
 
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        print("📍 LocationManager: Authorization status changed to \(status.rawValue)")
+	func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+		print("📍 LocationManager: Authorization status changed to \(status.rawValue)")
 
-        switch status {
-        case .authorizedWhenInUse, .authorizedAlways:
-            print("📍 Разрешение получено, запрашиваем локацию...")
-            locationManager.requestLocation()
-        case .denied, .restricted:
-            print("❌ Доступ к геолокации запрещен")
-            presenter.updateDistancesForCourts(initialCourts, userLocation: nil)
-        case .notDetermined:
-            print("📍 Статус авторизации не определен")
-        @unknown default:
-            print("❌ Неизвестный статус авторизации")
-            presenter.updateDistancesForCourts(initialCourts, userLocation: nil)
-        }
-    }
+		switch status {
+		case .authorizedWhenInUse, .authorizedAlways:
+			print("📍 Разрешение получено, запрашиваем локацию...")
+			locationManager.requestLocation()
+		case .denied, .restricted:
+			print("❌ Доступ к геолокации запрещен")
+			presenter.updateDistancesForCourts(initialCourts, userLocation: nil)
+		case .notDetermined:
+			print("📍 Статус авторизации не определен")
+		@unknown default:
+			print("❌ Неизвестный статус авторизации")
+			presenter.updateDistancesForCourts(initialCourts, userLocation: nil)
+		}
+	}
 }
 
 // MARK: - Private Methods
@@ -214,58 +235,60 @@ private extension CourtListViewController {
 		filteredCourts.count + (expandedIndex != nil ? 1 : 0)
 	}
 
-    func setupTable() {
-        tableView.dataSource = self
-        tableView.delegate = self
+	func setupTable() {
+		tableView.dataSource = self
+		tableView.delegate = self
 		tableView.register(CourtTableViewCell.self, forCellReuseIdentifier: CourtTableViewCell.reuseIdentifier)
 		tableView.register(CourtDetailsCell.self, forCellReuseIdentifier: CourtDetailsCell.reuseIdentifier)
-        tableView.backgroundColor = .clear
-    }
+		tableView.backgroundColor = .clear
+	}
 
-    func setupUI() {
+	func setupUI() {
 		view.backgroundColor = .clear
 		view.addSubviews(glassmorphismView, searchField, tableView)
 
 		let mainIndent: CGFloat = 20
 
-        NSLayoutConstraint.activate([
+		NSLayoutConstraint.activate([
 			glassmorphismView.topAnchor.constraint(equalTo: view.topAnchor, constant: 8),
 			glassmorphismView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
 			glassmorphismView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
-			glassmorphismView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+			glassmorphismView.bottomAnchor.constraint(equalTo: tableView.bottomAnchor),
 
 			searchField.topAnchor.constraint(equalTo: glassmorphismView.topAnchor, constant: mainIndent),
 			searchField.leadingAnchor.constraint(equalTo: glassmorphismView.leadingAnchor, constant: mainIndent),
 			searchField.trailingAnchor.constraint(equalTo: glassmorphismView.trailingAnchor, constant: -mainIndent),
 
-            tableView.topAnchor.constraint(equalTo: searchField.bottomAnchor, constant: 8),
-            tableView.leadingAnchor.constraint(equalTo: glassmorphismView.leadingAnchor, constant: mainIndent),
-            tableView.trailingAnchor.constraint(equalTo: glassmorphismView.trailingAnchor, constant: -mainIndent),
-			tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
-    }
+			tableView.topAnchor.constraint(equalTo: searchField.bottomAnchor, constant: 8),
+			tableView.leadingAnchor.constraint(equalTo: glassmorphismView.leadingAnchor, constant: mainIndent),
+			tableView.trailingAnchor.constraint(equalTo: glassmorphismView.trailingAnchor, constant: -mainIndent),
+		])
 
-    func setupLocation() {
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+		heightConstraint = tableView.heightAnchor.constraint(equalToConstant: 0)
+		heightConstraint.isActive = true
+	}
 
-        // Проверяем текущий статус авторизации
-        switch locationManager.authorizationStatus {
-        case .notDetermined:
-            print("📍 Запрашиваем разрешение на геолокацию...")
-            locationManager.requestWhenInUseAuthorization()
-        case .authorizedWhenInUse, .authorizedAlways:
-            print("📍 Разрешение получено, запрашиваем локацию...")
-            locationManager.requestLocation()
-        case .denied, .restricted:
-            print("❌ Доступ к геолокации запрещен пользователем")
-            // Показываем корты без расстояний
-            presenter.updateDistancesForCourts(initialCourts, userLocation: nil)
-        @unknown default:
-            print("❌ Неизвестный статус авторизации геолокации")
-            presenter.updateDistancesForCourts(initialCourts, userLocation: nil)
-        }
-    }
+	func setupLocation() {
+		locationManager.delegate = self
+		locationManager.desiredAccuracy = kCLLocationAccuracyBest
+
+		// Проверяем текущий статус авторизации
+		switch locationManager.authorizationStatus {
+		case .notDetermined:
+			print("📍 Запрашиваем разрешение на геолокацию...")
+			locationManager.requestWhenInUseAuthorization()
+		case .authorizedWhenInUse, .authorizedAlways:
+			print("📍 Разрешение получено, запрашиваем локацию...")
+			locationManager.requestLocation()
+		case .denied, .restricted:
+			print("❌ Доступ к геолокации запрещен пользователем")
+			// Показываем корты без расстояний
+			presenter.updateDistancesForCourts(initialCourts, userLocation: nil)
+		@unknown default:
+			print("❌ Неизвестный статус авторизации геолокации")
+			presenter.updateDistancesForCourts(initialCourts, userLocation: nil)
+		}
+	}
 
 	func setupSearchTextField() {
 		searchField.addTarget(self, action: #selector(searchTextChanged), for: .editingChanged)
