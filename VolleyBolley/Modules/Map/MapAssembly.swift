eed12834d1b3f0e9222192475a162f372abb0e2d
Roman Romanov <rromanov.vk@gmail.com>
2025-08-20 16:25:01 +0500

    [VB-18] adding MapAssembly

---
+++ VolleyBolley/Modules/Map/MapAssembly.swift
@@ -0,0 +1,29 @@
+//
+//  MapAssembly.swift
+//  VolleyBolley
+//
+//  Created by Roman Romanov on 20.08.2025.
+//
+
+import Swinject
+
+final class MapAssembly: Assembly {
+
+	func assemble(container: Container) {
+		container.register(MapViewController.self) { resolver in
+			let router = MapRouter()
+			let interactor = MapInteractor()
+
+			let presenter = MapPresenter(
+				interactor: interactor,
+				router: router
+			)
+
+			let view = MapViewController(presenter: presenter)
+			router.attachViewController(view)
+			presenter.view = view
+
+			return view
+		}
+	}
+}
