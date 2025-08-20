664b227434344f0e73f6bdced1afe36f22c0ee9d
Roman Romanov <rromanov.vk@gmail.com>
2025-08-20 15:37:42 +0500

    [VB-18] fixed code style

--- VolleyBolley/Modules/Map/MapRouter.swift
+++ VolleyBolley/Modules/Map/MapRouter.swift
@@ -14,11 +14,11 @@ protocol MapRouterProtocol: AnyObject {
 final class MapRouter: MapRouterProtocol {
 	
 	// MARK: - Private Properties
-	
+
 	private weak var listVC: CourtListViewController?
 	
 	// MARK: - Private Methods
-	
+
 	func showList(from view: MapViewController, courts: [CourtModel], selected: CourtModel?) {
 		let listVC = CourtListViewController(courts: courts, selected: selected)
 		view.addChild(listVC)
