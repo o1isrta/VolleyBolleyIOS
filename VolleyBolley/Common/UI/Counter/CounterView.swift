4fb332de08810c776ed74dc085c058c5fbbe0fa1
Demian Petropavlov <demyanpain@gmail.com>
2025-08-21 22:04:39 +0400

    [VB-68] Добавил CounterType

--- VolleyBolley/Common/UI/Counter/CounterView.swift
+++ VolleyBolley/Common/UI/Counter/CounterView.swift
@@ -5,4 +5,30 @@
 //  Created by Demain Petropavlov on 21.08.2025.
 //
 
-import Foundation
+import UIKit
+
+/// Тип счетчика: для игроков или для команд
+enum CounterType {
+    case players
+    case teams
+    
+    var minValue: Int {
+        switch self {
+        case .players: return Constants.minPlayers
+        case .teams: return Constants.minTeams
+        }
+    }
+    
+    var maxValue: Int {
+        Constants.maxValue
+    }
+    
+    // MARK: - Constants
+    private enum Constants {
+        static let minPlayers = 4
+        static let minTeams = 3
+        static let maxValue = 24
+    }
+}
+
+
