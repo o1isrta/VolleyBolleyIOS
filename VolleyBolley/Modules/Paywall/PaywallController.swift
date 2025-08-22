4cde4e9a288fd6260b9d285190f7e26be8371371
Roman Romanov <rromanov.vk@gmail.com>
2025-08-22 12:59:54 +0500

    [VB-61] init

---
+++ VolleyBolley/Modules/Paywall/PaywallController.swift
@@ -0,0 +1,20 @@
+//
+//  PaywallController.swift
+//  VolleyBolley
+//
+//  Created by Roman Romanov on 22.08.2025.
+//
+
+import UIKit
+
+final class PaywallController: BaseViewController {
+
+	// MARK: - Private Properties
+
+}
+
+// MARK: - Private Methods
+
+private extension PaywallController {
+
+}
