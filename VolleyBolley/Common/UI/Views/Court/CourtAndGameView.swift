341dace744b2cccbae3b258f05da9e546888f23d
Roman Romanov <rromanov.vk@gmail.com>
2025-08-24 09:47:26 +0500

    [VB-18] fixed translations

--- VolleyBolley/Common/UI/Views/Court/CourtAndGameView.swift
+++ VolleyBolley/Common/UI/Views/Court/CourtAndGameView.swift
@@ -178,12 +178,12 @@ import SwiftUI
 			game: GameModel.mockData,
 			hostType: .game,
 			doneButtonData: CourtButtonData(
-				title: "CHOOSE THIS GAME",
-				action: { print("aaaaaaa")}
+				title: String(localized: "CHOOSE THIS GAME"),
+				action: { print("aaaaaaa") }
 			),
 			detailsButtonData: CourtButtonData(
-				title: "Details",
-				action: { print("bbbbbbb")}
+				title: String(localized: "DETAILS"),
+				action: { print("bbbbbbb") }
 			)
 		)
 		view.configure(with: model)
@@ -201,8 +201,8 @@ import SwiftUI
 		let model = CourtViewModel(
 			court: CourtModel.mockData,
 			doneButtonData: CourtButtonData(
-				title: "Choose this court",
-				action: { print("aaaaaaa")}
+				title: String(localized: "CHOOSE THIS COURT"),
+				action: { print("aaaaaaa") }
 			)
 		)
 		view.configure(with: model)
--- VolleyBolley/Common/UI/Views/Court/CourtDetailsView.swift
+++ VolleyBolley/Common/UI/Views/Court/CourtDetailsView.swift
@@ -161,16 +161,16 @@ import SwiftUI
 
 		let model = GameDetailsViewModel(
 			court: CourtModel.mockData,
-			distance: "Nearest",
+			distance: String(localized: "Nearest"),
 			game: GameModel.mockData,
 			hostType: .game,
 			doneButtonData: CourtButtonData(
-				title: "CHOOSE THIS GAME",
-				action: { print("aaaaaaa")}
+				title: String(localized: "CHOOSE THIS GAME"),
+				action: { print("aaaaaaa") }
 				),
 			detailsButtonData: CourtButtonData(
-				title: "Details",
-				action: { print("bbbbbbb")}
+				title: String(localized: "DETAILS"),
+				action: { print("bbbbbbb") }
 			)
 		)
 		view.configure(with: model)
@@ -185,14 +185,14 @@ import SwiftUI
 		let view = CourtDetailsView()
 		let model = CourtDetailsViewModel(
 			court: CourtModel.mockData,
-			distance: "Nearest",
+			distance: String(localized: "Nearest"),
 			doneButtonData: CourtButtonData(
-				title: "CHOOSE THIS GAME",
-				action: { print("aaaaaaa")}
+				title: String(localized: "CHOOSE THIS GAME"),
+				action: { print("aaaaaaa") }
 				),
 			detailsButtonData: CourtButtonData(
-				title: "Details",
-				action: { print("bbbbbbb")}
+				title: String(localized: "DETAILS"),
+				action: { print("bbbbbbb") }
 			)
 		)
 		view.configure(with: model)
