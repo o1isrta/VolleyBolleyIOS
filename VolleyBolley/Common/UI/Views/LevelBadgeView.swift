9cc108c79b38ece4cda74fbbf4dea2c98526dcb5
EvdAnastasia <anastasia.evdokimovitch@yandex.ru>
2025-08-27 17:29:16 +0300

    [VB-69] Добавлен вью уровня в NavBar

--- VolleyBolley/Common/UI/Views/LevelBadgeView.swift
+++ VolleyBolley/Common/UI/Views/LevelBadgeView.swift
@@ -8,64 +8,65 @@
 import UIKit
 
 final class LevelBadgeView: UIView {
-    
+
     // MARK: - Constants
-    
+
     private enum Constants {
-        static let badgeSize: CGFloat = 132
-        static let badgeBorder: CGFloat = 2
-        static let badgeFontSize: CGFloat = 22
+        static let badgeSize: CGFloat = 46
+        static let badgeBorder: CGFloat = 1
+        static let badgeFontSize: CGFloat = 8
     }
-    
+
     // MARK: - Private Properties
-    
+
     private lazy var levelLabel: UILabel = {
         let label = UILabel()
         label.font = AppFont.Quantex.regular(size: Constants.badgeFontSize)
         label.textAlignment = .center
         return label
     }()
-    
+
     // MARK: - Initializers
-    
-    init(_ level: UserLevel) {
+
+    override init(frame: CGRect) {
         super.init(frame: .zero)
         setupView()
-        configure(with: level)
+        configure(with: .unknown)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override var intrinsicContentSize: CGSize {
         return CGSize(width: Constants.badgeSize, height: Constants.badgeSize)
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
         layer.cornerRadius = min(bounds.width, bounds.height) / 2
         layer.masksToBounds = true
+    }
+
+    // MARK: - Public Methods
+
+    func configure(with level: UserLevel) {
+        levelLabel.text = level.title
+        levelLabel.textColor = level.titleColor
+        backgroundColor = level.color
+
+        layer.borderWidth = Constants.badgeBorder
+        layer.borderColor = AppColor.Border.primary.cgColor
     }
 }
 
 // MARK: - Private Methods
 
 private extension LevelBadgeView {
-    
-    func configure(with level: UserLevel) {
-        levelLabel.text = level.title
-        levelLabel.textColor = level.titleColor
-        backgroundColor = level.color
-        
-        layer.masksToBounds = true
-        layer.borderWidth = Constants.badgeBorder
-        layer.borderColor = AppColor.Border.primary.cgColor
-    }
-    
+
     func setupView() {
         addSubviews(levelLabel)
-        
+
         NSLayoutConstraint.activate([
             levelLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
             levelLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
@@ -84,13 +85,15 @@ import SwiftUI
 #Preview {
     ZStack {
         Color(AppColor.Background.screen).ignoresSafeArea()
-        
-        VStack(spacing: 12) {
+
+        HStack(spacing: 12) {
             ForEach(UserLevel.allCases, id: \.self) { level in
                 UIViewPreview {
-                    LevelBadgeView(level)
+                    let view = LevelBadgeView()
+                    view.configure(with: level)
+                    return view
                 }
-                .frame(width: 132, height: 132)
+                .frame(width: 46, height: 46)
             }
         }
         .padding()
