0bfe73e5920c18078df19d461460b3c87ee5a224
Roman Romanov <rromanov.vk@gmail.com>
2025-08-31 18:57:06 +0500

    [VB-83] NotificationsViewController, init

---
+++ VolleyBolley/Modules/Notifications/NotificationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NotificationsViewController.swift
+//  VolleyBolley
+//
+//  Created by Roman Romanov on 31.08.2025.
+//
+
+import UIKit
+
+final class NotificationsViewController: BaseViewController {
+	
+}
+
+#if DEBUG
+@available(iOS 17.0, *)
+#Preview {
+	NotificationsViewController()
+}
+#endif
