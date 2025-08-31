ad8b03a30ef7fb54d0f0f1570ca57eeb73c905df
Roman Romanov <rromanov.vk@gmail.com>
2025-08-31 18:41:15 +0500

    [VB-83] adding UILabel.applyGradient

--- VolleyBolley/Common/Extensions/UILabel+Extensions.swift
+++ VolleyBolley/Common/Extensions/UILabel+Extensions.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 extension UILabel {
+
 	func setTextWithDifferentStyles(_ texts: [(String, UIFont)]) {
 		let attributedString = NSMutableAttributedString()
 
@@ -19,5 +20,21 @@ extension UILabel {
 		}
 
 		self.attributedText = attributedString
+	}
+
+	func applyGradient(
+		textLayerAlignmentMode: CATextLayerAlignmentMode = .left,
+		colors: [UIColor] = AppGradient.greenLight
+	) {
+		// Remove existing gradient layer if any
+		layer.sublayers?.forEach { $0.removeFromSuperlayer() }
+		// Ensure label has proper bounds
+		layoutIfNeeded()
+		guard !bounds.isEmpty else { return }
+		// Set text color to clear so gradient shows
+		textColor = .clear
+		// Create gradient layer with left-aligned text
+		let gradientLayer = CALayer.makeGradientTextMask(for: self, textLayerAlignmentMode: textLayerAlignmentMode)
+		layer.addSublayer(gradientLayer)
 	}
 }
