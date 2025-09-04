fbdcb92350b62b9133103ecedbf63fc69209967e
Roman Romanov <rromanov.vk@gmail.com>
2025-09-04 10:14:42 +0500

    [VB-83] refactoring to VIPER

---
+++ VolleyBolley/Modules/Notifications/NotificationsRouter.swift
@@ -0,0 +1,30 @@
+//
+//  NotificationsRouter.swift
+//  VolleyBolley
+//
+//  Created by Roman Romanov on 31.08.2025.
+//
+
+import UIKit
+
+protocol NotificationsRouterProtocol: AnyObject {
+	var viewController: UIViewController? { get set }
+	func navigateBack(from view: NotificationsViewControllerProtocol?)
+}
+
+final class NotificationsRouter: NotificationsRouterProtocol {
+
+	// MARK: - Public Properties
+
+	weak var viewController: UIViewController?
+
+	// MARK: - Public Methoda
+
+	func navigateBack(from view: NotificationsViewControllerProtocol?) {
+		if let viewController = viewController {
+			viewController.navigationController?.popViewController(animated: true)
+		} else if let view = view as? UIViewController {
+			view.navigationController?.popViewController(animated: true)
+		}
+	}
+}
