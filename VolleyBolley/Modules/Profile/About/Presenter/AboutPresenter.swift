b6567497a82324e7a0da0f6f07183cc5466be345
Demian Petropavlov <demyanpain@gmail.com>
2025-09-05 15:58:12 +0400

    [VB-82] Создана базовая структура

---
+++ VolleyBolley/Modules/Profile/About/Presenter/AboutPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  AboutPresenter.swift
+//  VolleyBolley
+//
+//  Created by Demain Petropavlov on 05.09.2025.
+//
+
+import Foundation
---
+++ VolleyBolley/Modules/Profile/About/Router/AboutRouter.swift
@@ -0,0 +1,8 @@
+//
+//  AboutRouter.swift
+//  VolleyBolley
+//
+//  Created by Demain Petropavlov on 05.09.2025.
+//
+
+import Foundation
