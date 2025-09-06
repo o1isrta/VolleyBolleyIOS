8362b3ab9df52414396b5c405b23a45c40f4a266
EvdAnastasia <anastasia.evdokimovitch@yandex.ru>
2025-09-06 21:10:19 +0300

    [VB-67] Реализована навигация между экранами профиля и данными

--- VolleyBolley/Modules/PersonalData/Router/PersonalDataRouter.swift
+++ VolleyBolley/Modules/PersonalData/Router/PersonalDataRouter.swift
@@ -9,13 +9,26 @@ import UIKit
 
 protocol PersonalDataRouterProtocol: AnyObject {
     func attachViewController(_ view: UIViewController)
+    func navigateBack(from view: PersonalDataViewControllerProtocol?)
 }
 
 final class PersonalDataRouter: PersonalDataRouterProtocol {
 
+    // MARK: - Public Properties
+
     weak var viewController: UIViewController?
+
+    // MARK: - Public Methods
 
     func attachViewController(_ view: UIViewController) {
         viewController = view
+    }
+
+    func navigateBack(from view: PersonalDataViewControllerProtocol?) {
+        if let viewController = viewController {
+            viewController.navigationController?.popViewController(animated: true)
+        } else if let view = view as? UIViewController {
+            view.navigationController?.popViewController(animated: true)
+        }
     }
 }
