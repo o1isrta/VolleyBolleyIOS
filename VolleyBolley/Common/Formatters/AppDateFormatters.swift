60fdbe2efddf0dbcc07291116dbeb546309f1b30
Roman Romanov <rromanov.vk@gmail.com>
2025-09-27 15:20:53 +0500

    [VB-119] fixed code style

--- VolleyBolley/Common/Formatters/AppDateFormatters.swift
+++ VolleyBolley/Common/Formatters/AppDateFormatters.swift
@@ -8,41 +8,41 @@
 import Foundation
 
 enum AppDateFormatters {
-
-    static let serverDateOnly: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd"
-        formatter.locale = AppConstants.AppLocale.posix
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
-        return formatter
-    }()
-
+	
+	static let serverDateOnly: DateFormatter = {
+		let formatter = DateFormatter()
+		formatter.dateFormat = "yyyy-MM-dd"
+		formatter.locale = AppConstants.AppLocale.posix
+		formatter.timeZone = TimeZone(secondsFromGMT: 0)
+		return formatter
+	}()
+	
 	static let apiDateOnly: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm"
-        formatter.locale = AppConstants.AppLocale.posix
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
-        return formatter
-    }()
-
-    static let localizedInterval: DateIntervalFormatter = {
-        let formatter = DateIntervalFormatter()
-        formatter.locale = Locale.current
-        formatter.timeZone = .current
-        formatter.dateStyle = .medium
-        formatter.timeStyle = .short
-        return formatter
-    }()
-
-    static let time12Hour: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "h:mm a"
-        formatter.amSymbol = "AM"
-        formatter.pmSymbol = "PM"
-        formatter.locale = AppConstants.AppLocale.posix
-        return formatter
-    }()
-
+		let formatter = DateFormatter()
+		formatter.dateFormat = "yyyy-MM-dd HH:mm"
+		formatter.locale = AppConstants.AppLocale.posix
+		formatter.timeZone = TimeZone(secondsFromGMT: 0)
+		return formatter
+	}()
+	
+	static let localizedInterval: DateIntervalFormatter = {
+		let formatter = DateIntervalFormatter()
+		formatter.locale = Locale.current
+		formatter.timeZone = .current
+		formatter.dateStyle = .medium
+		formatter.timeStyle = .short
+		return formatter
+	}()
+	
+	static let time12Hour: DateFormatter = {
+		let formatter = DateFormatter()
+		formatter.dateFormat = "h:mm a"
+		formatter.amSymbol = "AM"
+		formatter.pmSymbol = "PM"
+		formatter.locale = AppConstants.AppLocale.posix
+		return formatter
+	}()
+	
 	static let dateWithTime: DateFormatter = {
 		let formatter = DateFormatter()
 		formatter.locale = AppConstants.AppLocale.posix
@@ -50,7 +50,7 @@ enum AppDateFormatters {
 		formatter.dateFormat = "d MMMM, h:mm"
 		return formatter
 	}()
-
+	
 	static let time: DateFormatter = {
 		let formatter = DateFormatter()
 		formatter.timeZone = .current
@@ -58,13 +58,13 @@ enum AppDateFormatters {
 		formatter.dateFormat = "h:mm a"
 		return formatter
 	}()
-
-    static let onlyDate: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        formatter.timeStyle = .none
+	
+	static let onlyDate: DateFormatter = {
+		let formatter = DateFormatter()
+		formatter.dateStyle = .medium
+		formatter.timeStyle = .none
 		formatter.locale = AppConstants.AppLocale.posix
-        formatter.timeZone = .current
-        return formatter
-    }()
+		formatter.timeZone = .current
+		return formatter
+	}()
 }
