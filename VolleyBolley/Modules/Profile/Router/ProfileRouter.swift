c12e89b762d6342ae8ed0da80e8759c5e3e63f16
Roman Romanov <rromanov.vk@gmail.com>
2025-10-09 15:11:10 +0500

    [VB-134] ProfileRouter/ProfileAssembly, refactoring

--- VolleyBolley/Modules/Profile/Router/ProfileRouter.swift
+++ VolleyBolley/Modules/Profile/Router/ProfileRouter.swift
@@ -17,41 +17,56 @@ protocol ProfileRouterProtocol: AnyObject {
 
 final class ProfileRouter: ProfileRouterProtocol {
 
+	// MARK: - Public Properties
+
     weak var viewController: UIViewController?
+
+	// MARK: - Private Properties
+
+	private let personalDataViewController: () -> PersonalDataViewController?
+	private let supportViewController: () -> SupportViewController?
+	private let faqViewController: () -> FAQViewController?
+	private let aboutViewController: () -> AboutViewController?
+
+	// MARK: - Initializers
+
+	init(
+		personalDataViewController: @escaping () -> PersonalDataViewController?,
+		supportViewController: @escaping () -> SupportViewController?,
+		aboutViewController: @escaping () -> AboutViewController?,
+		faqViewController: @escaping () -> FAQViewController?
+	) {
+		self.personalDataViewController = personalDataViewController
+		self.supportViewController = supportViewController
+		self.aboutViewController = aboutViewController
+		self.faqViewController = faqViewController
+	}
+
+	// MARK: - Public Methods
 
     func attachViewController(_ view: UIViewController) {
         viewController = view
     }
 
     func showPersonalData() {
-        guard let personalDataVC = DIContainer.shared.resolver.resolve(PersonalDataViewController.self) else {
-            fatalError("PersonalDataViewController не зарегистрирован")
-        }
-
+		guard let personalDataVC = personalDataViewController() else {
+			fatalError("PersonalDataViewController could not be created")
+		}
         viewController?.navigationController?.pushViewController(personalDataVC, animated: true)
     }
 
 	func showSupport() {
-        guard let supportVC = DIContainer.shared.resolver.resolve(SupportViewController.self) else {
-            fatalError("SupportViewController не зарегистрирован")
-        }
-
+		guard let supportVC = supportViewController() else { fatalError("SupportViewController could not be created") }
 		viewController?.navigationController?.pushViewController(supportVC, animated: true)
     }
 
 	func showAbout() {
-		guard let aboutVC = DIContainer.shared.resolver.resolve(AboutViewController.self) else {
-			fatalError("AboutViewController не зарегистрирован")
-		}
-
+		guard let aboutVC = aboutViewController() else { fatalError("AboutViewController could not be created") }
 		viewController?.navigationController?.pushViewController(aboutVC, animated: true)
 	}
 
     func showFAQ() {
-        guard let faqVC = DIContainer.shared.resolver.resolve(FAQViewController.self) else {
-            fatalError("FAQViewController не зарегистрирован")
-        }
-
+		guard let faqVC = faqViewController() else { fatalError("FAQViewController could not be created") }
         viewController?.navigationController?.pushViewController(faqVC, animated: true)
     }
 }
