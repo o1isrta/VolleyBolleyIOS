14a2008a92320db4f4bfc89caf0a1e66d8a4c573
Roman Romanov <rromanov.vk@gmail.com>
2025-10-12 23:20:39 +0500

    fixed linter

--- VolleyBolley/Common/Extensions/UserDefaults.swift
+++ VolleyBolley/Common/Extensions/UserDefaults.swift
@@ -10,10 +10,10 @@ import Foundation
 extension UserDefaults {
     var isOnboardingShown: Bool {
         get {
-			bool(forKey: AppConstants.UserDefaults.Keys.onboardingShown)
+			bool(forKey: AppConstants.UserDefaults.onboardingShown)
         }
         set {
-            set(newValue, forKey: AppConstants.UserDefaults.Keys.onboardingShown)
+            set(newValue, forKey: AppConstants.UserDefaults.onboardingShown)
             synchronize()
         }
     }
