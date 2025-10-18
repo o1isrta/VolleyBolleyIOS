379e26ab6a8af04a58bb4465a8b5329bfc688ba0
Roman Romanov <rromanov.vk@gmail.com>
2025-10-18 18:15:31 +0500

    [VB-40] get time range from NewGameOrTourneyDateCell

---
+++ VolleyBolley/Modules/NewGameOrTourney/Entity/SelectedDateRange.swift
@@ -0,0 +1,13 @@
+//
+//  SelectedDateRange.swift
+//  VolleyBolley
+//
+//  Created by Roman Romanov on 18.10.2025.
+//
+
+import Foundation
+
+struct SelectedDateRange {
+	var startTime: Date
+	var endTime: Date
+}
