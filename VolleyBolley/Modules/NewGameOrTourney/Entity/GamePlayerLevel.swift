2882b1cb32a24c42b204b897e5d31c429435daa5
Roman Romanov <rromanov.vk@gmail.com>
2025-10-18 22:18:50 +0500

    [VB-40] adding NewGameOrTourneyPlayerLevelCell

---
+++ VolleyBolley/Modules/NewGameOrTourney/Entity/GamePlayerLevel.swift
@@ -0,0 +1,15 @@
+//
+//  GamePlayerLevel.swift
+//  VolleyBolley
+//
+//  Created by Roman Romanov on 18.10.2025.
+//
+
+import Foundation
+
+enum GamePlayerLevel {
+	case light
+	case medium
+	case hard
+	case pro
+}
