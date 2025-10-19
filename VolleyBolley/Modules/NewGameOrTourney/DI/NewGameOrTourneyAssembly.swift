06999db8dd0900a8d2c05da4c145cd7e8e1fc032
Roman Romanov <rromanov.vk@gmail.com>
2025-10-19 15:34:32 +0500

    [VB-40] remove NewGameOrTourneyInteractor

--- VolleyBolley/Modules/NewGameOrTourney/DI/NewGameOrTourneyAssembly.swift
+++ VolleyBolley/Modules/NewGameOrTourney/DI/NewGameOrTourneyAssembly.swift
@@ -14,14 +14,11 @@ final class NewGameOrTourneyAssembly: Assembly {
 
 	static func createModule(with parentViewController: UIViewController?) -> NewGameOrTourneyViewController {
 		let newGameOrTourneyVC = NewGameOrTourneyViewController()
-		let interactor = NewGameOrTourneyInteractor()
 		let router = NewGameOrTourneyRouter(viewController: newGameOrTourneyVC)
 		let presenter = NewGameOrTourneyPresenter(
-			interactor: interactor,
 			router: router
 		)
 		newGameOrTourneyVC.presenter = presenter
-		interactor.presenter = presenter
 		presenter.view = newGameOrTourneyVC
 
 		return newGameOrTourneyVC
@@ -32,14 +29,11 @@ final class NewGameOrTourneyAssembly: Assembly {
 	func assemble(container: Container) {
 		container.register(NewGameOrTourneyViewController.self) { _ in
 			let newGameOrTourneyVC = NewGameOrTourneyViewController()
-			let interactor = NewGameOrTourneyInteractor()
 			let router = NewGameOrTourneyRouter(viewController: newGameOrTourneyVC)
 			let presenter = NewGameOrTourneyPresenter(
-				interactor: interactor,
 				router: router
 			)
 			newGameOrTourneyVC.presenter = presenter
-			interactor.presenter = presenter
 			presenter.view = newGameOrTourneyVC
 
 			return newGameOrTourneyVC
