eac63bacf03a7951145b11432e1de843cd94680d
Roman Romanov <rromanov.vk@gmail.com>
2025-10-25 18:59:22 +0500

    [VB-80] VIPER compatibility + custom alert

--- VolleyBolley/Modules/EditProfilePhoto/Router/EditProfilePhotoRouter.swift
+++ VolleyBolley/Modules/EditProfilePhoto/Router/EditProfilePhotoRouter.swift
@@ -10,13 +10,15 @@ import UIKit
 protocol EditProfilePhotoRouterProtocol: AnyObject {
 	func attachViewController(_ view: UIViewController)
 	func navigateBack()
+	func showPhotoLibrary(delegate: LibraryPhotoPickerServiceDelegate)
+	func showCamera() throws(PhotoPickerError)
 }
 
 final class EditProfilePhotoRouter: EditProfilePhotoRouterProtocol {
 
 	// MARK: - Public Properties
 
-	weak var viewController: UIViewController?
+	private weak var viewController: UIViewController?
 
 	// MARK: - Public Methods
 
@@ -26,5 +28,27 @@ final class EditProfilePhotoRouter: EditProfilePhotoRouterProtocol {
 
 	func navigateBack() {
 		viewController?.navigationController?.popViewController(animated: true)
+	}
+
+	func showPhotoLibrary(delegate: LibraryPhotoPickerServiceDelegate) {
+		guard let viewController else { return }
+		let picker = LibraryPhotoPickerService()
+		picker.delegate = delegate
+		picker.modalPresentationStyle = .fullScreen
+		viewController.present(picker, animated: true)
+	}
+
+	func showCamera() throws(PhotoPickerError) {
+		guard let viewController else { return }
+
+		guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+			throw .cameraUnavailable
+		}
+
+		let imagePicker = UIImagePickerController()
+		imagePicker.sourceType = .camera
+		imagePicker.delegate = viewController as? (UIImagePickerControllerDelegate & UINavigationControllerDelegate)
+		imagePicker.allowsEditing = true
+		viewController.present(imagePicker, animated: true)
 	}
 }
