83f09ff2ded71a9f4c134888fd384e124802e879
Nikolai Eremenko <nikolai.dev@icloud.com>
2025-11-06 09:04:25 +0400

    [VB-107] refactor: auth module

--- VolleyBolley/Modules/Flows/Auth/DI/AuthAssembly.swift
+++ VolleyBolley/Modules/Flows/Auth/DI/AuthAssembly.swift
@@ -13,35 +13,33 @@ final class AuthAssembly: Assembly {
     func assemble(container: Container) {
         container.register(AuthRouterProtocol.self) { resolver in
             AuthRouter(
-                window: resolver.safeResolve(UIWindow.self),
+                window: resolver.resolve(UIWindow.self)!,
                 viewControllerFactory: {
-                    resolver.safeResolve(AuthViewProtocol.self)
+                    resolver.resolve(AuthViewProtocol.self)!
                 },
                 userRegFactory: {
-                    resolver.safeResolve(UserRegViewProtocol.self)
+                    resolver.resolve(UserRegViewProtocol.self)!
                 }
             )
         }
         .inObjectScope(.container)
 
         container.register(AuthViewProtocol.self) { resolver in
-            let googleAuthService = resolver.safeResolve(GoogleAuthServiceProtocol.self)
-            let firebaseAuthService = resolver.safeResolve(FirebaseAuthServiceProtocol.self)
-            let authRepository = resolver.safeResolve(AuthRepositoryProtocol.self)
-
-            let router = resolver.safeResolve(AuthRouterProtocol.self)
+            let googleAuthService = resolver.resolve(GoogleAuthServiceProtocol.self)!
+            let firebaseAuthService = resolver.resolve(FirebaseAuthServiceProtocol.self)!
+            let authRepository = resolver.resolve(AuthRepositoryProtocol.self)!
+            let sessionRepository = resolver.resolve(SessionRepositoryProtocol.self)!
+            let router = resolver.resolve(AuthRouterProtocol.self)!
             let interactor = AuthInteractor(
                 googleAuthService: googleAuthService,
                 firebaseAuthService: firebaseAuthService,
                 authRepository: authRepository,
+                sessionRepository: sessionRepository,
                 router: router
             )
             let presenter = AuthPresenter(
                 interactor: interactor,
-                router: router,
-                //                finishAuthFlow: { [weak router] in
-                //                    router?.finishAuth()
-                //                }
+                router: router
             )
             let view = AuthViewController(presenter: presenter)
 
