7b101f482ea0c197498dca2af3bf5e794075e553
Roman Romanov <rromanov.vk@gmail.com>
2026-01-03 18:07:23 +0500

    [VB-163] fixed typo

--- VolleyBolley/Modules/Support/View/SupportViewController.swift
+++ VolleyBolley/Modules/Support/View/SupportViewController.swift
@@ -263,7 +263,7 @@ extension SupportViewController: MFMailComposeViewControllerDelegate {
 	) {
 		controller.dismiss(animated: true)
 		guard result == .sent else { return }
-		let message = String(localized: "emailToSupport.succesfullySent")
+		let message = String(localized: "emailToSupport.successfullySent")
 		showAlert(with: message)
 	}
 }
